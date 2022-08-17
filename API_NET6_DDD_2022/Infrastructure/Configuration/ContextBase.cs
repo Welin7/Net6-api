33d613b18b52d2b9b54cee27208baa177bad8613
Welington Dias <33883532+Welin7@users.noreply.github.com>
2022-08-17 15:23:18 -0300

    Net6 Api with DDD.
    Implementation the class RepositoryGenerics, RepositoryMessage and the class of ServiceMessage with your interfaces.

---
+++ API_NET6_DDD_2022/Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,46 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+
+        }
+    
+        public DbSet<Message> Message { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }   
+    
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(getConnectionString());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(u => u.Id);
+            base.OnModelCreating(builder);
+        }
+
+        
+        public string getConnectionString()
+        {
+            return "Server=WELIN\\SQL2019,1433;Database=ApiNet6;User Id=sa;Password=medsys;";
+        }   
+    }
+}
