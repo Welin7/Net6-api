ba9e75f64fdebe7a4ff144980a3697ddb1711ba2
Welington Dias <33883532+Welin7@users.noreply.github.com>
2022-08-30 19:53:04 -0300

    Net6 Api with DDD.
    Create project of web api. Configuration token jwt and include view model of Login and MessageViewModel.

---
+++ API_NET6_DDD_2022/WebAPIs/Token/TokenJwt.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace WebAPIs.Token
+{
+    
+    public class TokenJwt
+    {
+        private JwtSecurityToken token;
+        internal TokenJwt(JwtSecurityToken token)
+        {
+            this.token = token;
+        }
+
+        public DateTime ValidToTo => token.ValidTo;
+        
+        public string value => new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
